406528fd6939d997881745d906e4d48f54d5f106
Yuchi <62748666+ba144220@users.noreply.github.com>
2025-10-25 01:37:30 -0700

    Revert "Feat/basic chatroom (#2)"
    This reverts commit 0ed47a288e1d08c9a530cea697293bf95be04ccc.

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from "wxt";
 
-import tailwindcss from "@tailwindcss/vite";
-
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
-  manifest: {
-    permissions: ["sidePanel"],
-  },
-
-  entrypointsDir: "src/entrypoints",
 });
--- server/main.py
+++ server/main.py
@@ -1,4 +1,15 @@
-from src.main import main
+"""Server main file"""
+
+# Internal imports
+import os
+
+# External imports
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
 
 if __name__ == "__main__":
-    main()
+
+    port = int(os.getenv("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
