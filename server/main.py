b9745da3a242ac6b26e886d001b9b887298b8084
Yuchi Hsu <yuchihsu@stanford.edu>
2025-10-24 22:45:37 -0700

    setup: black formatter

--- server/main.py
+++ server/main.py
@@ -1,9 +1,15 @@
+"""Server main file"""
+
+# Internal imports
 import os
+
+# External imports
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
 if __name__ == "__main__":
-    import uvicorn
+
     port = int(os.getenv("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
